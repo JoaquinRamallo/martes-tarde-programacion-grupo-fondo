6ed109933eec471ffa885b1d350bb715aa3f3faa
Cruzlobo05 <clobo@uade.edu.ar>
2026-04-14 17:04:50 -0300

    Delete 5. BLOQUE 4 directory

--- 5. BLOQUE 4/interfaces/DiccionarioSimpleTDA.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-public interface DiccionarioSimpleTDA {
-    void InicializarDiccionario();
-    void Agregar(int clave, int valor);
-    void Eliminar(int clave);
-    int[] Claves();
-}
-
